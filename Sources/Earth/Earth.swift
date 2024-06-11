863d7c7434ed5b2a8e494be8bb558a13ddf0faa6
Tomohiro Kumagai <tomo@ez-net.jp>
2024-06-11 19:19:23 +0900

    Allows RawRepresenting macro export any name of constant definitions.

--- Sources/Earth/Earth.swift
+++ Sources/Earth/Earth.swift
@@ -1,8 +1,8 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-@attached(member, names: named(init(rawValue:)), named(rawValue))
+@attached(member, names: named(init(rawValue:)), named(rawValue), arbitrary)
 public macro RawRepresenting<RawType>(constantPrefix: String = "") = #externalMacro(module: "EarthMacros", type: "RawRepresenting")
 
-@attached(member, names: named(init(rawValue:)), named(rawValue))
+@attached(member, names: named(init(rawValue:)), named(rawValue), arbitrary)
 public macro MutableRawRepresenting<RawType>(constantPrefix: String = "") = #externalMacro(module: "EarthMacros", type: "MutableRawRepresenting")
